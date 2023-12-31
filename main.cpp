fb1337e63d71764949ea6797bb22832061e8ec72
Kevin Patel <kxvin.pxtel1@gmail.com>
2023-12-31 15:05:29 -0500

    Created new function to print hello world.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+//Function Declaration
+void helloWorld();
+
+
+//Main Function
 int main(){
 
-    
+    helloWorld();
 
+}
+
+//Function prints Hello World
+void helloWorld(){
+
+    std::cout << "Hello World\n";
 
 }
